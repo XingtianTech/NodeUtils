40c2b7d2cfc739d4614e7841b4ea4fea4e674c27
pickgliss <pickgliss@gmail.com>
2023-10-26 09:49:54 +0800

    RedisSessionOptions

--- Middlewares/RedisSession.ts
+++ Middlewares/RedisSession.ts
@@ -4,30 +4,24 @@ import {createClient} from "redis"
 
 const redisClient = createClient()
 redisClient.connect().catch(console.error)
+export type RedisSessionOptions = {prefix:string,secret:string,name:string}
 
-
-// let redisStore = new RedisStore({
-//     client: redisClient,
-//     // disableTouch:true,
-//     prefix: "xingtian",
-//   })
-  
-  export const RedisSession = ({prefix,secret,name}:any)=>
-  {
-    return session({
-      store: new RedisStore({ client: redisClient,  prefix: prefix, }) ,
-      secret: secret,
-      saveUninitialized: false,
-      resave: false,
-      name: name,
-      cookie: {
-        secure: false, // if true: only transmit cookie over https, in prod, always activate this
-        httpOnly: true, // if true: prevents client side JS from reading the cookie
-        maxAge: 1000 * 60 * 30, // session max age in milliseconds
-        // explicitly set cookie to lax
-        // to make sure that all cookies accept it
-        // you should never use none anyway
-        sameSite: 'lax',
-      },  
-    });
-  } 
+export const RedisSession = ({prefix,secret,name}:RedisSessionOptions)=>
+{
+  return session({
+    store: new RedisStore({ client: redisClient,  prefix: prefix, }) ,
+    secret: secret,
+    saveUninitialized: false,
+    resave: false,
+    name: name,
+    cookie: {
+      secure: false, // if true: only transmit cookie over https, in prod, always activate this
+      httpOnly: true, // if true: prevents client side JS from reading the cookie
+      maxAge: 1000 * 60 * 30, // session max age in milliseconds
+      // explicitly set cookie to lax
+      // to make sure that all cookies accept it
+      // you should never use none anyway
+      sameSite: 'lax',
+    },  
+  });
+} 

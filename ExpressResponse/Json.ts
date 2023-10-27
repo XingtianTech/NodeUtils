5337efdc8d60eac41f2791a15167d497ca912e03
pickgliss <pickgliss@gmail.com>
2023-10-27 09:29:00 +0800

    ExpressResponse

--- ExpressResponse/Json.ts
+++ ExpressResponse/Json.ts
@@ -2,7 +2,6 @@ import {NextFunction, Request, Response} from 'express';
 
 export default function (req: Request, res: Response, next: NextFunction) {
     const {status = 200,json} = res.locals;
-    console.log("HandleToUnity  send result ,",json);
     if(json)
     {
         res.status(status).json(json);

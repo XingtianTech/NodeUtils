f2d813ab7f83b13892d4fa90b155f42f3ed24fa0
pickgliss <pickgliss@gmail.com>
2023-10-27 16:07:59 +0800

    error message

--- ExpressResponse/Err.ts
+++ ExpressResponse/Err.ts
@@ -1,13 +1,12 @@
 import {NextFunction, Request, Response} from 'express';
 export default function (err: any, req: Request, res: Response, next: NextFunction) {
-    console.log("Error catch!!")
     if(typeof err ==="string" )
     {
         res.status(500).json({error:err});
     }else if(typeof err === 'object')
     {
         const {status = 500,message} = err;
-        console.log("Error object1111:",err)
+        console.log(err)
         res.status(status).json({error:message});
     }
 }
--- dist/ExpressResponse/Err.js
+++ dist/ExpressResponse/Err.js
@@ -1,13 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function default_1(err, req, res, next) {
-    console.log("Error catch!!");
     if (typeof err === "string") {
         res.status(500).json({ error: err });
     }
     else if (typeof err === 'object') {
         const { status = 500, message } = err;
-        console.log("Error object1111:", err);
+        console.log(err);
         res.status(status).json({ error: message });
     }
 }
